7805302ba3f403f1c89adfe2fc96974fd1fe0dae
Jose Morillos <jmorillosp@pucp.edu.pe>
2023-11-05 02:40:52 -0500

    Vista MisJuegos terminada

--- Lab11_GTICS_Base_2023-2/src/main/java/edu/pucp/gtics/lab11_gtics_20232/dao/JuegosDao.java
+++ Lab11_GTICS_Base_2023-2/src/main/java/edu/pucp/gtics/lab11_gtics_20232/dao/JuegosDao.java
@@ -28,10 +28,10 @@ public class JuegosDao {
        return lista;
    }
 
-   public List<Juegos> listarMisJuegos(Integer idUsuario){
+   public List<JuegosxUsuario> listarMisJuegos(Integer idUsuario){
        RestTemplate restTemplate = new RestTemplate();
        String url = "http://localhost:8081/api/juegos/listaMisJuegos?id="+idUsuario;
-       ResponseEntity<Juegos[]> responseEntity = restTemplate.getForEntity(url, Juegos[].class);
+       ResponseEntity<JuegosxUsuario[]> responseEntity = restTemplate.getForEntity(url, JuegosxUsuario[].class);
        return Arrays.asList(responseEntity.getBody());
    }
 
